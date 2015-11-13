cba886c060f929db6d250f602e25e4a82637ac18
Jeffrey Martin <Animeman1999@gmail.com>
2015-11-12 23:01:17 -0500

    Wired up the Fragment

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageActivity.java
@@ -1,5 +1,7 @@
 package edu.kvcc.cis298.cis298assignment3;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +13,13 @@ public class BeverageActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = new BeverageFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageFragment.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageFragment.java
@@ -4,17 +4,52 @@ package edu.kvcc.cis298.cis298assignment3;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
 /**
  * Created by jmartin5229 on 11/12/2015.
  */
 public class BeverageFragment extends Fragment {
     private Beverage mBeverage; // To hold a single Beverage class
+    private EditText mTitlefield;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBeverage = new Beverage();
-  }
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_wine, container, false);
+        mTitlefield = (EditText)v.findViewById(R.id.beverage_title);
+        mTitlefield.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBeverage.setName(s.toString());//      This needs more data added to the mBeverage
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        return v;
+    }
+
 
 
 }
