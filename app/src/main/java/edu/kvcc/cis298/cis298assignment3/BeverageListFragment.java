d12b95192c22c95ff0428840e8442c8187a8d432
Jeffrey Martin <Animeman1999@gmail.com>
2015-11-15 22:42:35 -0500

    Added beverage adpater

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageListFragment.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Jeffrey on 11/15/2015.
@@ -19,7 +22,43 @@ public class BeverageListFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_beverage_list, container, false);
         mBeverageRecyclerView = (RecyclerView) view.findViewById(R.id.beverage_recycler_view);
         mBeverageRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        
+
         return view;
+    }
+
+    private class BeverageHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+
+        public BeverageHolder (View itemView){
+            super(itemView);
+            mTextView = (TextView) itemView;
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<BeverageHolder>{
+
+        private List<Beverage> mBeverages;
+
+        public CrimeAdapter(List<Beverage> beverages){
+            mBeverages = beverages;
+        }
+
+        @Override
+        public BeverageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+            return new BeverageHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(BeverageHolder holder, int position){
+            Beverage beverage = mBeverages.get(position);
+            holder.mTextView.setText(beverage.getId());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBeverages.size();
+        }
     }
 }
