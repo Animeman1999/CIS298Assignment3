55711aa036bc201e2be01b103db30c00e6ad34ff
Jeffrey Martin <Animeman1999@gmail.com>
2015-11-19 22:50:41 -0500

    Fixes data modification. Bug in taking out the last number of price.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageFragment.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageFragment.java
@@ -4,12 +4,16 @@ package edu.kvcc.cis298.cis298assignment3;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.CompoundButton;
+
 
 /**
  * Created by jmartin5229 on 11/12/2015.
@@ -93,18 +97,88 @@ public class BeverageFragment extends Fragment {
 
         mBeverageTitle = (EditText)v.findViewById(R.id.beverage_title);
         mBeverageTitle.setText(mBeverage.getName());
+        mBeverageTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Space left intentionally blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBeverage.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Space left intentionally blank
+            }
+        });
 
         mIdNum= (EditText)v.findViewById(R.id.id_num);
         mIdNum.setText(Integer.toString(mBeverage.getId()));
+        mIdNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Space left intentionally blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBeverage.setId(Integer.parseInt(s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Space left intentionally blank
+            }
+        });
 
         mPack = (EditText)v.findViewById(R.id.pack);
         mPack.setText(mBeverage.getPack());
+        mPack.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Space left intentionally blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBeverage.setPack(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Space left intentionally blank
+            }
+        });
 
         mPrice = (EditText)v.findViewById(R.id.price);
         mPrice.setText(String.valueOf(mBeverage.getPrice()));
+        mPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Space left intentionally blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBeverage.setPrice(Double.parseDouble(s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Space left intentionally blank
+            }
+        });
 
         mActive = (CheckBox)v.findViewById(R.id.active);
         mActive.setChecked(mBeverage.getActive());
+        mActive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mBeverage.setActive(isChecked);
+            }
+        });
 
 
         return v;
